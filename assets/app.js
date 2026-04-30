c4808f87a01e4fb393e89ea1b0c1ed8bbf487e76
Iker <iker.perez@udc.es>
2026-04-30 23:49:27 +0200

    feat: add GitHub repo link to header

--- assets/app.js
+++ assets/app.js
@@ -680,12 +680,24 @@ function buildSectionMenu(activeSections) {
     .join("");
 
   return `
+    ${githubRepoLinkMarkup()}
     <details class="section-menu">
       <summary><span class="section-menu-trigger">Sections</span></summary>
       <div class="section-menu-panel">
         <div class="section-menu-grid">${groups}</div>
       </div>
     </details>
+  `;
+}
+
+function githubRepoLinkMarkup() {
+  return `
+    <a class="repo-link" href="https://github.com/ikerperez12/UI-IP-Toolkit-v4.0" target="_blank" rel="noopener noreferrer" aria-label="Open UI IP Toolkit repository on GitHub">
+      <svg viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.39 7.86 10.91.58.1.79-.25.79-.56v-2.02c-3.2.7-3.88-1.37-3.88-1.37-.52-1.34-1.28-1.7-1.28-1.7-1.05-.72.08-.71.08-.71 1.16.08 1.77 1.19 1.77 1.19 1.03 1.77 2.7 1.26 3.36.96.1-.75.4-1.26.73-1.55-2.55-.29-5.23-1.28-5.23-5.68 0-1.25.45-2.28 1.18-3.08-.12-.29-.51-1.46.11-3.04 0 0 .96-.31 3.16 1.18A10.9 10.9 0 0 1 12 6.14c.98 0 1.96.13 2.88.39 2.19-1.49 3.15-1.18 3.15-1.18.63 1.58.24 2.75.12 3.04.74.8 1.18 1.83 1.18 3.08 0 4.41-2.69 5.38-5.25 5.67.41.36.78 1.06.78 2.14v3.17c0 .31.21.67.8.56A11.51 11.51 0 0 0 23.5 12C23.5 5.65 18.35.5 12 .5Z"/>
+      </svg>
+      <span>GitHub</span>
+    </a>
   `;
 }
 
